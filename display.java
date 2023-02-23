8d86a9e0dd00429cdb9dd58f7947bacbb98e2278
YisusBlack18 <jesusangel18jz@gmail.com>
2023-02-23 11:49:58 -0700

    Comentarios

--- display.java
+++ display.java
@@ -3,6 +3,9 @@ import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+// Interfaz Grafica del Juego de la Vida
+// Autor:   Jesus Angel Juarez Zazueta
+
 public class display extends JFrame {
 
     private JLabel label;
@@ -15,29 +18,34 @@ public class display extends JFrame {
     protected JButton clearButton;
     protected Thread game;
 
+    // Inicia la logica del programa
     public static void main(String[] args) {
         gamelife.main(args);
 
     }
-    
+
+    // Contructor de la interfaz
     public display() {
+        // Se inicializan las propiedades de la ventana principal
         setLayout(new BorderLayout(5,5));
         setBounds(10,10,800,800);
         setTitle("Game of Life");
         setResizable(false);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
+        // Propiedades del titulo
         label = new JLabel("Bienvenido al Juego de la Vida");
         label.setBounds(10,10,600,100);
         fuente = new Font("Comic Sans", Font.BOLD, 24);
         label.setFont(fuente);
         add(label,BorderLayout.NORTH);
 
+        // Inicializacion del panel principal de la matriz
         panel = new JPanel();
         panel.setLayout(new GridLayout(gamelife.M,gamelife.N));
 
+        // Manda a crear la matriz(tabla) de botones y la agrega al panel principal
         matriz = gamelife.creaMatriz();
-
         for (int i = 0; i < gamelife.M; i++) {
             for (int j = 0; j < gamelife.N; j++) {
                 panel.add(matriz[i][j]);
@@ -47,22 +55,27 @@ public class display extends JFrame {
 
         add(panel,BorderLayout.CENTER);
         
+        // Se inicializa el panel de control del programa
         panel2 = new JPanel();
         panel2.setLayout(new GridLayout(1,2));
 
         fuente = new Font("Comic Sans", Font.PLAIN, 18);
         
+        // Se crea el boton de incio que da comienzo a un hilo que ejecutara la logica del programa constatemente hasta detenerlo
+        // presionando nuevamente el boton o limpiando la matriz y se añade al panel de control
         startButton = new JButton("Start");
         startButton.setFont(fuente);
         startButton.setPreferredSize(new Dimension(30,50));
         startButton.addActionListener(new ActionListener() {
            public void actionPerformed (ActionEvent e) {
+                // Inicio del hilo de ejecucion
                 Runnable oullea = new Runnable() {
                     public void run() {
                         try {
                             if (startButton.getText() == "Start") {
                                 startButton.setText("Stop");
-                                
+                                // Manda a llamar la funcion que genera la nueva instancia del matriz 
+                                // segun la logica del problema y la agrega al panel
                                 while (startButton.getText() == "Stop") {
                                     matriz = gamelife.nextGeneration(matriz,gamelife.M,gamelife.N);
                                     panel.removeAll();
@@ -88,9 +101,9 @@ public class display extends JFrame {
                 game.start();
            }
         });
-
         panel2.add(startButton);
 
+        // Se crea el boton que ejecuta la logica del programa manualmente paso a paso y se añade al panel de control
         nexButton = new JButton("Next");
         nexButton.setFont(fuente);
         nexButton.setPreferredSize(new Dimension(30,50));
@@ -111,6 +124,7 @@ public class display extends JFrame {
         });
         panel2.add(nexButton);
         
+        // Se crea el boton que limpia la matriz y tambien detiene el programa
         clearButton = new JButton("Clear");
         clearButton.setFont(fuente);
         clearButton.setPreferredSize(new Dimension(30,50));
@@ -129,12 +143,14 @@ public class display extends JFrame {
         });
         panel2.add(clearButton);
 
+        // Añade el panel de control a la ventana principal
         add(panel2,BorderLayout.SOUTH);
 
         setVisible(true);
         
     }
 
+    // Funcio que limita la velocidad de ejecucion de la logica
     public void espera() {
         try {
             Thread.sleep(500);
--- gamelife.java
+++ gamelife.java
@@ -4,25 +4,32 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 
+// Logica del Juego de la Vida
+// Autor:   Jesus Angel Juarez Zazueta
+
 public class gamelife {
 
     public static int M = 10;
     public static int N = 10;
 
+    // Inicia la interfaz grafica del programa
     public static void main(String[] args) {
         new display();
     }
 
+    // Crea la matriz de botones
     public static JButton[][] creaMatriz() {
         JButton[][] matriz = new JButton[M][N];
 
         for (int i = 0; i < M; i++) {
             for (int j = 0; j < N; j++) {
+                // Se crean los botones en cada espacio del matriz y se definen sus propiedades
                 matriz[i][j] = new JButton();
                 matriz[i][j].setBackground(Color.GRAY);
                 matriz[i][j].setPreferredSize(new Dimension(10,10));
                 matriz[i][j].addActionListener(new ActionListener() {
-
+                    // Agrega un actionlistener que cambia el color del boton cuando se presiona
+                    // Si esta muerta(desactivada) se mantiene en gris, si esta viva cambia a amarillo
                     @Override
                     public void actionPerformed(ActionEvent e) {
                         if (((JButton) e.getSource()).getBackground() == Color.GRAY) {
@@ -38,15 +45,18 @@ public class gamelife {
 
         return matriz;
     }
-
+    // Funcion que recibe la matriz actual, genera y devuelve una nueva segun la logica del problema
+    // Nota: Las casillas fuera de los limites de la matriz se consideran muertas
     public static JButton[][] nextGeneration(JButton[][] tabla, int M, int N) {
         JButton[][] nextTabla = creaMatriz();
 
         for (int i = 0; i < M; i++) {
             for (int j = 0; j < N; j++) {
                 int vivos = 0;
+                // Se recorren las casillas(botones) una por una
                 for (int k = -1; k <= 1; k++) {
                     for (int k2 = -1; k2 <= 1; k2++) {
+                        // Cuenta las casillas vivas alrededor de la casilla actual dentro de los limites
                         if ((i+k>=0 && i+k<M) && (j+k2>=0 && j+k2<N)) {
                             if (tabla[i+k][j+k2].getBackground() == Color.YELLOW) {
                                 vivos++;
@@ -54,20 +64,27 @@ public class gamelife {
                         }
                     }
                 }
+                // Reducimos el conteo si la casilla central(enfocada) esta viva
                 if (tabla[i][j].getBackground() == Color.YELLOW) {
                     vivos--;
                 }
+                // Si la casilla central(enfocada) esta muerta, dependiendo del numero de casillas vivas alrededor se hace la siguiente:
                 if (tabla[i][j].getBackground() == Color.GRAY) {
+                    // Si hay 3 vivas exactamente la casilla en cuestion vive(se activa), de otra forma se mantiene muerta
                     if (vivos == 3) {
                         nextTabla[i][j].setBackground(Color.YELLOW);
                     } else {
                         nextTabla[i][j].setBackground(Color.GRAY);
                     }
+                // Si la casilla esta viva, se hace lo siguiente
                 } else {
+                    // Si hay alrededor 1 o menos casillas vivas, la casilla muere
                     if (vivos <= 1) {
                         nextTabla[i][j].setBackground(Color.GRAY);
+                    // Si entre 2 o 3 casillas vivas, la casilla se mantiene viva
                     } else if (vivos >= 2 && vivos <= 3) {
                         nextTabla[i][j].setBackground(Color.YELLOW);
+                    // Si hay 4 o mas casillas vivas, la casilla se muere
                     } else if (vivos >= 4) {
                         nextTabla[i][j].setBackground(Color.GRAY);
                     }
